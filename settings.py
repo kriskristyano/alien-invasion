d067d3e71fb775dcde4953810bba361bfaf926e2
ck <ckep218@gmail.com>
2025-09-17 19:51:52 -0500

    Implement ship speed setting

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
